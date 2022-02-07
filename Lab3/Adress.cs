4e79bad4d0f719c801b8d283b96df68c7419e6c2
Alex Xenon <b3haply@gmail.com>
2022-02-07 12:23:50 +0200

    Added Laborator 3

---
+++ Lab3/Adress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Lab3
+{
+    public class Address
+    {
+        private static int count = 0;
+        public int id { get; private set; }
+        private string street { get; set; }
+        private int block { get; set; }
+        private int apartament { get; set; }
+
+        public Address(string street, int block, int apartament)
+        {
+            this.id = ++count;
+            this.street = street;
+            this.block = block;
+            this.apartament = apartament;
+        }
+
+        public override string ToString()
+        {
+            return $"{street} {block}/{apartament}";
+        }
+    }
+ 
+}
---
+++ Lab3/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp.Lab3
+{
+    public class Employee
+    {
+        public static int count = 0;
+        public int id;
+        public string name;
+        public bool qualified;
+        public int salary;
+        public Address address;
+
+        public Employee(string name, bool qualified, int salary, Address address)
+        {
+            this.id = ++count;
+            this.name = name;
+            this.qualified = qualified;
+            this.salary = salary;
+            this.address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {id}, Name: {name}, Qualified: {(qualified ? "Yes" : "Nope")}, Salary: {salary}, Address: {address}";
+        }
+    }
+}
